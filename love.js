3cff185fce0346481cf004162c471de55471a8ff
timmybutter <108182983+timmybutter@users.noreply.github.com>
2022-06-25 22:00:08 +0800

    Add files via upload

---
+++ love.js
@@ -0,0 +1,434 @@
+var c = 0;
+
+var when = ["smile as you make timmy dance", 
+"get all serious about workshop safety",
+"shake your bootayyyy",
+"do not judge my late night instant noodle cravings",
+"do the floppy hug with me",
+"hum",
+"cry out in terror when I start putting my weight on you",
+"say goodnighty to me multiple times a day",
+"nerd out!",
+"squeak as you sneeze",
+"get shy and hide your face using otter hands",
+"MAWR MAWR MAWR MAWR at me ",
+"delight at new stickers",
+"tell me about the doggos you see on your walkie walks",
+"share with me your wip",
+"rejoice at finding corporate loopholes",
+"sing showtunes with me",
+"bring timmy out on excursions",
+"send chinese new year greetings to my mum ",
+"hold me steady when im drunk",
+"get scandalised when you see me wearing your shirt",
+"hug me",
+"get excited about eating bingsu",
+"let your feelings be known through google reviews",
+"help your friends :)",
+"get to execute your brilliant ideas",
+"ask me about my day ",
+"listen to me ramble on about my day",
+"tell me when you're going to poop!",
+"gush about your silky underwear",
+"blush :3",
+"lose to me in munchkins",
+"say HAIII when i wake up",
+"call me out on my sexism",
+"smile with your eyes",
+"offer to wash my hair",
+"stand up for your friends",
+"share lychee cheesecake with me",
+"pat timmy's butt multiple times a day!",
+"play thumb war with me competitively",
+"told me im your best friend",
+"give me silly pet names ",
+"imitate my single finger typing",
+"look super cool when you're focused",
+"acknowledge that you have sausagee arms",
+"pretended to be the JAL customer representative",
+"randomly dance with me",
+"send me reddit posts that made you laugh",
+"giggle",
+"piggyback me",
+"call people out on their bs",
+"paylah me with customised messages",
+"put on cologne ",
+"are clear about your boundaries",
+"never get sick of eating chocolate granola",
+"cycle without both hands",
+"laugh with strangers",
+"always make me ice milo if i ask",
+"point out ugly design to me",
+"roast almonds for salad",
+"explain why you're maddo during an argument",
+"offer to make the jellycats accessories",
+"taught yourself how to sew with a manual",
+"are comfortable with your masculinity",
+"get slightly offended that i can't tell when you shave",
+"name your wifi the WWJCN",
+"tell alexa to shut up",
+"explain to me traffic rules even though it'll go right outta my head",
+"tell me i look nice in an outfit",
+"accompany me as i do work",
+"take funny photos of me and giggle to yourself",
+"remember a small detail from the past and say, 'see i remember!' as you point to your forehead",
+"are proud of yourself",
+"can tell read my expressions no matter how slight",
+"offer to swap drinks with me",
+"make sqiggly drawings",
+"hold my hand in different ways",
+"respect my belief in the supernatural even though you don't",
+"and i talk endlessly without feeling like a drag",
+"didn't downplay my worry when i lost sally",
+"tell me about your depression",
+"cried really hard when we said goodbye at your doorstep",
+"offer to be my portable handwarmer",
+"talk about our future ",
+"cheer me up by sending me food and/or bubble tea",
+"send me silly photos of timmy and cookie sunning",
+"feel bad about timmy overexerting himself with russian twists",
+"get excited about new coffee",
+"facetime me while cleaning your apartment",
+"tell me about your day",
+"aren't jealous and insecure when i hang out with male friends",
+"trust me completely",
+"are slightly scarred by my finger over camera trick and is now always a little suspicious of me",
+"think im tricking you and indeed i am",
+"let me exert dominance over your leg",
+"flex your strong thighs :O",
+"go birding with your friends",
+"have a blue tape ready for anything",
+"organise your tools with the labeller",
+"remember that my favourite flower is hydrangeas",
+"proudly put the flowers into your ceramic black pot",
+"fall asleep more easily when timmy is on you",
+"randomly place jellycats on me when i get knocked out",
+"wear funky prints",
+"call me stinky pants",
+"show that you're in a fighting mode by turning your bag to the birb patch",
+"read my work",
+"never make casual promises",
+"pout when i called you weird for adding broccoli to nasi lemak ",
+"make fun of the way i hold my utensils",
+"speak chinese with a precise but somehow accented way",
+"whistle",
+"hold me tightly when im crumbly",
+"roll your eyes at my antics",
+"trim your nails, making your hands look honest and trustworthy",
+"have clay stuck in your fingernails ",
+"let me play with your peepee",
+"lose to me in balancing despite having sturdy hobbit feet",
+"don't mind when i wear high heels",
+"boop my nose",
+"make up song lyrics",
+"listen to me intently when i explain something",
+"take me seriously",
+"chuckle at my mispronunciations",
+"widen your eyes whenever you taste something good",
+"say 'why you bully me???' in a whiny manner",
+"compete seriously whenever we play made up sea games at sentosa",
+"sing 'sentossaaaaa'",
+"draw me a manatee tail on the sand",
+"read with me on the beach",
+"apply sunscreen on me in a non-sexual way",
+"let me marvel at your smooth back",
+"watch the manatees with me for a long time",
+"laughed so hard you had to squat down",
+"lie on my tummy",
+"sit on my thighs",
+"let me lie on top of your back",
+"struggle to get me off of you",
+"sigh in exasperation when you have to pull me up",
+"don't make me feel bad if i get things wrong",
+"know the directions just from reading the map once",
+"allow me to put the full weight of my chin on your hands",
+"let me massage your butt",
+"send me a scary pockets song",
+"play my boobs with childlike wonder",
+"occasionally send me deranged voice messages",
+"always forgive me for being late",
+"make pottery for your friends and family",
+"give me amazing shoulder massages",
+"filmed yourself doing different stretches for pain relief as demo",
+"pull timmy up like a daikon",
+"tell me what timmy smells like",
+"get worried if timmy absorbs the kitchen smell",
+"act kawaii",
+"are endlessly patient with me",
+"rewatch boogie's stories multiple times and get the same amount of enjoyment anyway",
+"comfort me but don't allow me to wallow",
+"laugh heartily",
+"encourage me",
+"appreciate both tea and coffee without feeling like you have to stick to one",
+"ask your mum for her banana bread recipe",
+"worry about your parents",
+"check up on me when we have sex",
+"remind me to pee after sex",
+"stick to your guts even though it's not a popular opinion",
+"gamely answers my hypothetical questions",
+"say, 'the real question is...'",
+"change your telegram profile pic to a jellycat",
+"made me mashed potato in a tupperware",
+"worry about my potassium level and ask if im eating too many potatoes and bananas",
+"automatically feel good after consuming burrata",
+"wear the goose shirt ",
+"ask to hear about my dreams",
+"comfort me when the thunder gets loud",
+"tuck me in",
+"smile at me as i drift off to sleep",
+"sent mcdonalds a positive feedback after tasting a quality cup of ice milo",
+"got upset when your therapist didn't comment on your otter shirt",
+"acknowledge that im doreamon when my hand becomes a fist",
+"tell me you miss me",
+"ask me to feed you when i send you a photo of yakun food",
+"delight at receiving haribo gummy bears",
+"use timmy as your beard",
+"draw otters and jellycats in your cards",
+"randomly hug my head",
+"tell me im cute",
+"sing to me",
+"pronounce compoooter",
+"explain technical things in way that's easy to understand",
+"change your lock screen to a photo of us",
+"make lame jokes and call yourself out on it",
+"say that you're beardy and somewhat domesticated",
+"love me for who i am",
+"let me squish you",
+"let me push your butt as you walk up the stairs",
+"gossip with me :D",
+"get squirmy when i share disturbing stickers",
+"tease me about my strawberry oats obsession",
+"say, 'tada!' with flourish",
+"drum on the steering wheel during traffic",
+"sing showtunes with me in the car",
+"let me bite your arm when we have sex",
+"scrub your toes in the shower",
+"make things with your hands!",
+"peek up to see how im doing when you eat me out :O",
+"order more mcnuggets because you know we can always finish them",
+"communicate with a chain of boogie stickers",
+"say 'fuck' softly when you tell me how good i feel :O",
+"try to imitate my high pitch voice and failing",
+"suggest new date ideas",
+"lie on the grass with me",
+"hold my hand while we cycle side by side",
+"wrap your arms around my waist",
+"tell me that you love me",
+"introduce me as your girlfriend",
+"put our photo in your living room and bedroom",
+"wave hi to me using the shiba inu kitchen glove",
+"try your best to resuscitate your succulent",
+"send me silly catto videos",
+"help me friends",
+"write your packing list in google sheets",
+"give me constructive criticism about my writing",
+"cheer when i flash me bewbies",
+"offer to sew my loose button ",
+"gently pat the cattos in the cat cafe",
+"call me pastry related names",
+"rub timmy's tummy",
+"let me hug your tummy",
+"converse with your grandma in hokkein",
+"talk to christine about boogie",
+"make random twitches when you sleep",
+"appreciate my cooking",
+"tried your best to make a kite",
+"dry my hair with a towel",
+"let me draw on your arm",
+"paint the bread people in my toaster tattoo",
+"pat my butt to sleep ",
+"offered me the better bike",
+"help me put on the cycling helmet",
+"rarely give up even when you feel discouraged",
+"laugh at timmy's soaked state",
+"fiddle with totoro's dumpling package",
+"amuse yourself easily",
+"lift me up for the mermaid pose",
+"have two crowd lu songs as your ear worms",
+"take it in stride when i tease you about being shorter than me",
+"nag at me to wash my bed toys",
+"treat my nightmares seriously",
+"show me your cat ear shaped nostrils whenever i ask",
+"pat me head virtually over facetime",
+"call the lizard in my room mr lizard",
+"rather i be bald than get a mullet",
+"indulge me when i ask to sit at the table with the grandma standee",
+"get amused by sally's sitted position",
+"hold timmy's paws to sleep",
+"celebrate your achievements :-)",
+"send me photos of your crazy floofy hair",
+"make timmy churn butter",
+"let me squeeze your bewbies",
+"smell like laundry",
+"offer to split airplane tickets with me",
+"end the sentence with :O when you're shy",
+"smile at doggos that walk by",
+"tell me i look nice even when i dont feel like it",
+"fall asleep on the floor with me",
+"offer to sleep on the inflated mattress",
+"trust me with your vulnerable self",
+"share an angsty scream with paul",
+"lend sheepie and timmy to me when i feel down",
+"wear matching pyjamas with me",
+"also eat heaps of green onions",
+"wrap a scarf snuggly around my neck",
+"warm my hands up by rubbing your hands together and enclosing yours over mine",
+"tell me you enjoy a bellycomics strip",
+"always have cash ready when we go to a kopitiam",
+"tell me what you learned from npr or bbc",
+"suggest that we donate to dumb friends league for boogie's birthday",
+"remove your mask and declare, 'it is i!'",
+"cheer for me no matter how small the win is",
+"kiss me :)",
+"call this smiley the double chin smiley :))",
+"do something for me every international women's day",
+"thanked sally, timmy, and i in your thesis",
+"believe in me even when i don't feel so shiny myself",
+"told me you just wanna have a plan to be together",
+"didn't expect that id have to follow you, but you can follow me overseas too",
+"get excited about eating song fa and roast paradise",
+"say goodbye to me by opening and closing your hands",
+"accepted me wholeheartedly when i came out as bisexual",
+"talk about how you wanna be the old man who walks his doggo every day",
+"pronounce nice man",
+"refer to yourself in third person as beardy man",
+"compliment yourself by going, your bf is quite good at xxx one hor.",
+"go SHEEESHHH",
+"suggested that we start an instagram account to rate ice milo quality across mcdonalds' in sg",
+"say majideh in a somewhat patronising-teasing way",
+"go wow i didnt know that in a genuine tone",
+"correct my posture by rolling my shoulders back",
+"flirt with me by batting your eyelashes",
+"go BURRRITOOO when you have good news",
+"pronounce half naked man in a funny way",
+"always offer to buy tissue packets from the seller",
+"accompanied me to fix my bike",
+"bought me bike accessories for my safety",
+"give me advice on tech gadgets to buy",
+"accompanied me to fix my speakers",
+"remember we're floofy butt when indoors and floofy pants when outdoors",
+"let me put my cheeks against your bare butt",
+"spend a long time choosing candles",
+"fed me biscuits :3 in the cold",
+"worry about me",
+"go, 'aiyoh girl ah!'",
+"let me wear your clothes and shoes",
+"tell me to buy more panties",
+"offer to pay for condom and lube and not expect me to take oral contraceptives",
+"appreciate my idiosyncracies",
+"reassure me that im not your manic pixie dream girl",
+"are silly",
+"look at me with warmth",
+"update the waffle app with the photos of fat pigeons and fluffy snow",
+"send me photos of the boston sky",
+"turn red and tipsy",
+"share your google drive space with me",
+"fostered my plants",
+"tell me you'd be busy so you can't text as much",
+"write to me, even when there's no special occasion",
+"ask me if im okie",
+"your signs of spring is spotting bunnies in the campus",
+"politely ask for more blanket",
+"go ahhhhhh when i do freeze attack with my cold feet",
+"go to picnics with me",
+"always plan my birthdays way in advance :)",
+"wobble",
+"pinned my voice message of going ribbit cuz it made you lol",
+"make my day better just by smiling at me",
+"start the facetime with an extreme closeup of your face (or timmy!)",
+"respect my ambivalence about having kids",
+"thank bob san for bringing you snackkos",
+"point out range rovers to me and ask if i like that model",
+"call masao wada your idol cuz of his fuzzy sweater",
+"stand up for yourself",
+"say superlatives like mostest comfy",
+"feel like home even from a distance"];
+
+
+$("._total").text(when.length);
+$("#progress").attr("max", when.length);
+
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+// shuffle on start
+$(function () {
+  shuffle(when);
+});
+
+$("main").click(function (e) {
+  // Check for left button
+  if (e.button == 0) {
+
+
+    if (c >= when.length) {
+
+      $("<p>love me too </p>").hide().prependTo("#when-you").fadeIn(2000);
+
+      $(".slider").fadeOut();
+
+      $("body").css('background', '#000');
+      $("#g p").css('color', '#fff');
+
+      $("main").unbind("click");
+
+      return;
+
+    }
+
+
+    $("<p>" + when[c] + "</p>").hide().prependTo("#when-you").fadeIn(2000);
+
+
+    updateSlider();
+
+    c++;
+  }
+
+});
+
+// progress listener
+$(document).on('input', '#progress', function () {
+
+  var prev = parseInt(c);
+  var diff = 0;
+
+  c = parseInt($(this).val());
+  updateSlider();
+
+  console.log(c + " prev:" + prev);
+
+  if (prev > c) { // remove
+    
+    diff = prev - c;
+    console.log("Remove", diff);
+
+    for (let i = 0; i < diff; i++) {
+      $("#when-you").find('p').first().remove();
+    }
+
+  } else { // add 
+    
+    diff = c - prev;
+    console.log("Increase", diff);
+
+    for (let i = 0; i < diff; i++) {
+      let n = parseInt(prev) + i;
+      console.log(n + " " + when[n]);
+      $("<p>" + when[n] + "</p>").hide().prependTo("#when-you").fadeIn(2000);
+    }
+    
+  }
+
+
+  $("#progress").attr("value", $(this).val());
+
+});
+
+
+function updateSlider() {
+  $("._done").text(c);
+  $("#progress").attr("value", c);
+}
